beacb731fdcd30d0cd74ec50676ccb2768949e5c
Максим Долгих <dolgih.m1996@gmail.com>
2023-02-05 20:11:41 +0000

    Update angular.json and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { NgContentComponent } from './lesson/ng-content/example/ng-content.component';
-import { MyCardWithContentComponent } from './lesson/ng-content/task/my-card-with-content/my-card-with-content.component';
-import { NgContentTaskComponent } from './lesson/ng-content/task/ng-content-task/ng-content-task.component';
-import 'zone.js';
+import { AppComponent } from "./app.component";
+import { NgContentComponent } from "./lesson/ng-content/example/ng-content.component";
+import { MyCardWithContentComponent } from "./lesson/ng-content/task/my-card-with-content/my-card-with-content.component";
+import { NgContentTaskComponent } from "./lesson/ng-content/task/ng-content-task/ng-content-task.component";
 
 @NgModule({
   declarations: [
