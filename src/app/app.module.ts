749b257f2966877c1760528af9f4c75be4ab6c0b
Misterion96 <30429266+Misterion96@users.noreply.github.com>
2023-02-05 23:59:02 +0400

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HostBindingComponent } from './lesson/host-binding/host-binding.component';
-import { HostListenerComponent } from './lesson/host-listener/host-listener.component';
 import { NgContentComponent } from './lesson/ng-content/example/ng-content.component';
-import {
-  MyCardWithContentComponent
-} from './lesson/ng-content/task/my-card-with-content/my-card-with-content.component';
+import { MyCardWithContentComponent } from './lesson/ng-content/task/my-card-with-content/my-card-with-content.component';
 import { NgContentTaskComponent } from './lesson/ng-content/task/ng-content-task/ng-content-task.component';
+import 'zone.js';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgContentComponent,
-    HostListenerComponent,
-    HostBindingComponent,
     MyCardWithContentComponent,
-    NgContentTaskComponent
+    NgContentTaskComponent,
   ],
-  imports: [
-    CommonModule,
-    BrowserModule
-  ],
+  imports: [CommonModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
