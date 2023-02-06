e2036fd7cae45c3bb6955c674e0dcea43fcde5a3
Maksim Dolgih <dolgih.m1996@gmail.com>
2023-02-06 10:00:12 +0400

    updated ng-container

--- src/app/lesson/ng-container/ng-container.component.ts
+++ src/app/lesson/ng-container/ng-container.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ng-container.component.less']
 })
 export class NgContainerComponent implements OnInit {
+  show = true;
   constructor() { }
 
   ngOnInit(): void {
