15133e0414b1f9bf513550cc5beb19b11a697611
Maksim Dolgih <dolgih.m1996@gmail.com>
2023-02-20 10:51:58 +0400

    added examples for form-control component

---
+++ src/app/lesson-15/examples/form-control/form-control.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control',
+  templateUrl: './form-control.component.html',
+  styleUrls: ['./form-control.component.less']
+})
+export class FormControlComponent implements OnInit {
+  // init
+  // get value - ngModelChange, valueChanges, value
+  // updateOn - change, blur, submit
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
