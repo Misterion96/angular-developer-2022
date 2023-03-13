547c20a86acfd7d34b16b2cde130ddebc0969bb6
Maksim Dolgih <dolgih.m1996@gmail.com>
2023-03-13 10:22:27 +0400

    feat: добавлен storage service

---
+++ src/app/lesson-testing/examples/services/storage/storage.service.spec.ts
@@ -0,0 +1,19 @@
+import { StorageService } from './storage.service';
+
+describe('StorageService', () => {
+    let service!: StorageService
+
+    beforeEach(() => {
+        service = new StorageService({ } as any)
+    })
+
+    it('get item', () => {
+
+    });
+
+    it('set item 1', function () {
+
+    });
+
+
+})
